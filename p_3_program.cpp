8e21cbb00bc35f5dbcc3c621958dd5d871aa7d08
asrahim88 <asrahim162@gmail.com>
2025-11-16 14:23:39 +0600

    program3

---
+++ p_3_program.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main() {
+    long long int a;
+    double b;
+    cout << "Please enter a big integer number: ";
+    cin >> a;
+    
+    cout << "Please Enter a big float number: ";
+    cin >> b;
+
+    cout <<"The big integer number is : "<< a <<"\n";
+    cout <<"The big float number is : "<< b;
+
+    return 0;
+}
