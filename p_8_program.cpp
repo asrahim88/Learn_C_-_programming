3e954ecd4bbf5707bca820674bd6fa577c562a76
asrahim88 <asrahim162@gmail.com>
2025-11-17 21:33:00 +0600

    program8

---
+++ p_8_program.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int* printAray() {
+    int *aray = new int[5];
+    for(int i =0; i<5; i++) {
+        cin >> aray[i];
+    }
+    
+    return aray;
+}
+int main() {
+    int *A = printAray();
+    for(int i = 0; i<5; i++) {
+        cout << A[i] << " " ;
+    }
+}
