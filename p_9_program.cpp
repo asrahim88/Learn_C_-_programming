dbf0be567fc9bd934e353d6f2d6446db24593e70
asrahim88 <asrahim162@gmail.com>
2025-11-19 19:20:20 +0600

    program9

---
+++ p_9_program.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main() {
+    int N;
+    cin >> N;
+
+    int *A = new int[N];
+    for(int i = 0; i<N; i++) {
+        cin >> A[i];
+    }
+
+    for (int i = 0; i < N; i++)
+    {
+        cout << A[i] << " ";
+    }
+    cout << endl;
+    int *B = new int[10];
+    for(int i=0; i<N;i ++ ){
+        B[i] = A[i];
+    }
+    for(int i = N; i<10; i++){
+        cin >> B[i];
+    }
+    delete [] A;
+    for(int i = 0; i<10; i++) {
+        cout << B[i] << " ";
+    }
+    cout << endl;
+    
+     
+}
