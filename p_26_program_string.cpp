5e67d20f148816b56a5452eda569858f37ecee4b
asrahim88 <asrahim162@gmail.com>
2025-11-27 11:02:23 +0600

    some string funtion

---
+++ p_26_program_string.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    string s;
+    cin >> s;
+    cout << s << endl;
+    cout << s.size() << endl;
+    s.resize(20, 'm');
+    cout << s << endl;
+    cout << s.front() << endl;
+    cout << s[0] << endl;
+    cout << s.back() << endl;
+    cout << s[s.size()-1] << endl;
+    cout << s.at(4) << endl;
+    cout << s.at(s.size()-1) << endl;
+    s.clear();
+    cout << s.size();
+    return 0;
+}
