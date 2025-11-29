055e19b26cdee5bf786e685e51601d26bd76b0ed
asrahim88 <asrahim162@gmail.com>
2025-11-29 11:27:27 +0600

    program28

--- p_28_program_string.cpp
+++ p_28_program_string.cpp
@@ -13,13 +13,44 @@ int main() {
     // string s("hello ");
     // cout << s << endl;
 
-    string s = "hello world";
-    string t(s, 3);
-    cout << t.size() << endl;
-    cout << t << endl;
+    // string s = "hello world";
+    // string t(s, 3);
+    // cout << t.size() << endl;
+    // cout << t << endl;
 
-    string s(10, 'B');
-    cout << s.size() << endl;
-    cout << s << endl;
+    // string s(10, 'B');
+    // cout << s.size() << endl;
+    // cout << s << endl;
+
+    // string name;
+    // getline(cin, name);
+
+    // cout << name.size() << endl;
+    // cout << name << endl;
+
+    // sort(name.begin(), name.end());
+    // cout << name << endl;
+    // reverse(name.begin(), name.end());
+    // cout << name << endl;
+
+    // sort(name.begin(), name.end());
+    // cout << name << endl;
+
+    // string name ;
+    // getline(cin, name);
+
+    // for(char c:name) {
+    //     cout << c << " ";
+    // }
+
+    // cout << endl;
+    // sort(name.begin(), name.end());
+    // cout << name << endl;
+
+    // for(char c: name) {
+    //     cout << c << " ";
+    // }
+
+
     return 0;
 }
