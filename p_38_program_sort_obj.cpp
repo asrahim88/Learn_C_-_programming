0d07ef7838da21211c4ee7d04d762fbc7815c7b8
asrahim88 <asrahim162@gmail.com>
2025-11-30 16:50:42 +0600

    sort object practice

---
+++ p_38_program_sort_obj.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+class StudentInfo{
+    public:
+    string name;
+    int roll;
+    char section;
+    string subject;
+    int marks;
+};
+bool cmp(StudentInfo l, StudentInfo r) {
+    return l.marks < r.marks ? true: false; 
+}
+int main() {
+    int N;
+    cin >> N;
+    
+    StudentInfo info[N];
+
+    for(int i =0; i<N ;i++){
+        cin.ignore();
+        getline(cin, info[i].name);
+        cin >> info[i].roll >> info[i].section >> info[i].subject >> info[i].marks;
+    }
+
+    sort(info, info+N, cmp);
+
+    for(int i =0; i<N; i++) {
+        cout << info[i].name << " " << info[i].roll << " " << info[i].section << " " << info[i].subject << " " << info[i].marks << endl;
+    }
+    return 0;
+}
