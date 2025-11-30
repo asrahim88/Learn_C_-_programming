2263e0484cfbea811367b74f0feef5c036f55c2c
asrahim88 <asrahim162@gmail.com>
2025-11-30 14:29:06 +0600

    program35

---
+++ p_35_program.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    string s("Hello");
+    cout << s << endl;
+
+    string s1("hello word", 4);
+    cout << s1 << endl;
+
+    string text = "hello world";
+    string text2(text, 2);
+    cout << text2 << endl;
+    return 0;
+}
