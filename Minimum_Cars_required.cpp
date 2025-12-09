7c8c228a7fd93f201419e63ca9377653e8f094aa
asrahim88 <asrahim162@gmail.com>
2025-12-09 23:42:12 +0600

    Minimum_Cars

---
+++ Minimum_Cars_required.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int T;
+	cin >> T;
+	
+	while(T--){
+	    int N;
+        cin >> N;
+
+        float result = ceil(N/4.0);
+        
+        cout << result << endl;
+	}
+
+}
