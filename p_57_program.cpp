c729865d82da0dae947b3fe02faebcd5be848fdc
asrahim88 <asrahim162@gmail.com>
2025-12-17 20:59:47 +0600

    program57

---
+++ p_57_program.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main() {
+    // vector<int> v;
+    // cout << v.size() << endl;
+    // vector<int> v(10);
+    vector<int> v(10, -5);
+    // for(int i=0; i<v.size(); i++) {
+    //     cout << v[i] << endl;
+    // }
+    // vector<int> v1(v);
+    // for(int i =0; i<v1.size(); i++) {
+    //     cout << v1[i] << endl;
+    // }
+    // vector<int> v2 = {10,20,30,40,50,60};
+    vector<int> v3 = {10,30,50};
+    cout << v3.size() << endl;
+    return 0;
+}
