4c8ad68933d47cc6c3b3d1e0424e5aaa135df61e
asrahim88 <asrahim162@gmail.com>
2025-12-20 19:42:30 +0600

    program57

--- p_57_program.cpp
+++ p_57_program.cpp
@@ -13,12 +13,32 @@ int main() {
     //     cout << v1[i] << endl;
     // }
     // vector<int> v2 = {10,20,30,40,50,60};
-    vector<int> v3 = {10,30,50};
-    v3.resize(5);
-    for(int i =0; i<v3.size();i ++){
-        cout << v3[i] << endl;
-    }
-    v3.clear();
+    // vector<int> v3 = {10,30,50};
+    // v3.resize(5);
+    // for(int i =0; i<v3.size();i ++){
+    //     cout << v3[i] << endl;
+    // }
+    // v3.clear();
 
+    vector<int> v;
+    // v.push_back(10);
+    // v.push_back(20);
+    // v.push_back(30);
+    // v.push_back(30);
+    // v.push_back(30);
+    // cout << v.capacity() << endl;
+    // cout << v.size() <<endl;
+    // v.clear();
+    // cout << v.size() << endl;
+    // cout << v[0] <<endl;
+    // cout << v[1] <<endl;
+    // cout << v[2] <<endl;
+    // cout << v[3] <<endl;
+    v.push_back(10);
+    // cout << v[0] << endl;
+    // cout << v[1] << endl;
+    for(int i =0; i<v.size(); i++) {
+        cout << v[i] <<endl; 
+    }
     return 0;
 }
