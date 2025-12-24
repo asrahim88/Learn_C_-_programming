848cf01523d139c997793fd50f7efa1a58c7825d
asrahim88 <asrahim162@gmail.com>
2025-12-24 10:46:41 +0600

    Insert at tail in tail optimize version

---
+++ p_69_program_insert_at_optimize.cpp
@@ -0,0 +1,65 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Node{
+    public:
+    int val;
+    Node* next;
+
+    Node(int val){
+        this->val = val;
+        this->next = NULL;
+    }
+};
+
+void insertAtHead(Node* &head, int val){
+    Node* newNode = new Node(val);
+
+    newNode->next = head;
+    head = newNode;
+}
+
+void insertAtAnyPosition(Node* head, int idx, int val){
+    Node* newNode = new Node(val);
+
+    Node* temp = head;
+    for(int i =1; i<idx; i++){
+        temp = temp->next;
+    }
+
+    newNode->next = temp->next;
+    temp->next = newNode;
+    
+}
+
+void insertAtTail(Node* head,Node* tail, int val){
+    Node* newNode = new Node(val);
+
+    if(head == NULL){
+        head = newNode;
+        return;
+    }
+    tail->next = newNode;
+    tail = newNode;
+}
+
+void printNode(Node* head){
+    Node* temp = head;
+
+    while(temp !=NULL){
+        cout << temp->val << " ";
+        temp = temp->next;
+    }
+}
+
+int main() {
+    Node* head = new Node(10);
+    Node* tail = head;
+
+    insertAtTail(head,tail, 20);
+    insertAtHead(head, 30);
+    insertAtAnyPosition(head, 3, 50);
+    insertAtAnyPosition(head, 1, 100);
+    insertAtTail(head, tail, 500);
+    printNode(head);
+    return 0;
+}
