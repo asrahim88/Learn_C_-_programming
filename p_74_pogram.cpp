28666e93576ffc81ef50a2085ed171c1d44837cf
asrahim88 <asrahim162@gmail.com>
2025-12-27 11:21:27 +0600

    linked list

---
+++ p_74_pogram.cpp
@@ -0,0 +1,67 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Node{
+    public:
+    int val;
+    Node* next;
+
+    Node(int val){
+        this->val = val;
+        this->next = NULL;
+    }
+};
+void insertAtHead(Node* &head, int val){
+    Node* newNode = new Node(val);
+    newNode->next = head;
+    head = newNode;
+}
+
+void insertAtAnyPosition(Node* head, int idx, int val){
+    Node* newNode = new Node(val);
+
+    Node* temp = head;
+    for(int i =1; i<idx; i++){
+       temp =  temp->next;
+    }
+    newNode->next = temp->next;
+    temp->next = newNode;
+}
+ 
+void insertAtTail(Node* &head, Node* &tail, int val){
+    Node* newNode = new Node(val);
+    if(head == NULL){
+        head = newNode;
+        tail = newNode;
+        return;
+    }
+
+    tail->next = newNode;
+    tail = newNode;
+}
+
+void printNode(Node* head){
+    Node* temp = head;
+
+    while (temp != NULL)
+    {
+        cout << temp->val << " ";
+        temp = temp->next;
+    }
+}
+int main() {
+    Node* head = NULL;
+    Node* tail = NULL;
+
+    while(true){
+        int x;
+        cin >> x;
+
+        if(x == -1){
+            break;
+        }
+
+        insertAtTail(head, tail, x);
+    }
+    printNode(head);
+    return 0;
+}
