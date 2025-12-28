cffac582cfd093c2961b5706a9cc3e0449ceb8af
asrahim88 <asrahim162@gmail.com>
2025-12-28 19:39:10 +0600

    program82

---
+++ p_82_program.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int N;
+    cin >> N;
+
+    int A[N];
+    for(int i=0;i <N; i++){
+        cin >> A[i];
+    }
+
+    long long int pre[N];
+    pre[0] = A[0];
+    for(long long int i=1; i<N;i ++){
+        pre[i] = pre[i-1] + A[i];
+    }
+    for(long long int i=N-1; i>=0; i--){
+        cout << pre[i] << " ";
+    }
+    return 0;
+}
