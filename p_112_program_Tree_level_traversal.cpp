8e9d683504e97b106bcc47cb1f54bed0704b4780
asrahim88 <asrahim162@gmail.com>
2026-01-26 11:31:39 +0600

    Level wise traversal tree data structure

---
+++ p_112_program_Tree_level_traversal.cpp
@@ -0,0 +1,75 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Node{
+    public:
+        int val;
+        Node* right;
+        Node* left;
+        Node(int val){
+            this->val = val;
+            this->left = NULL;
+            this->right = NULL;
+        }
+};
+
+void preOrderTraversal(Node* root){
+    if(root == NULL) return;
+    cout << root->val << " ";
+    preOrderTraversal(root->left);
+    preOrderTraversal(root->right);
+}
+void inOrderTraversal(Node* root){
+    if(root == NULL) return;
+    inOrderTraversal(root->left);
+    cout << root->val << " ";
+    inOrderTraversal(root->right);
+}
+
+void postOrderTraversal(Node* root){
+    if(root == NULL) return ;
+    postOrderTraversal(root->left);
+    postOrderTraversal(root->right);
+    cout << root->val << " ";
+}
+
+void levelOrderTraversal(Node* root){
+    queue<Node*> q;
+    q.push(root);
+
+    while (!q.empty())
+    {
+        Node* f = q.front();
+        q.pop();
+        cout << f->val << " ";
+        if(f->left != NULL){
+            q.push(f->left);
+        }
+        if(f->right != NULL){
+            q.push(f->right);
+        }
+    }
+    
+}
+int main() {
+    Node* root = new Node(10);
+    Node* a = new Node(20);
+    Node* b = new Node(30);
+    Node* c = new Node(40);
+    Node* d = new Node(50);
+    Node* e = new Node(60);
+
+    root->left = a;
+    root->right = b;
+    a->left = c;
+    b->left = d;
+    b->right = e;
+
+    preOrderTraversal(root);
+    cout << endl;
+    inOrderTraversal(root);
+    cout << endl;
+    postOrderTraversal(root);
+    cout << endl;
+    levelOrderTraversal(root);
+    return 0;
+}
