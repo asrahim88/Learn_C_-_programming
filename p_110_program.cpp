b5465fb185858d998cea465ce4fc0d92c9aed9ce
asrahim88 <asrahim162@gmail.com>
2026-01-26 09:34:18 +0600

    tree traversal in In-Order mode

---
+++ p_110_program.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Node{
+    public:
+        int val;
+        Node* left;
+        Node* right;
+
+        Node(int val){
+            this->val = val;
+            this->right = NULL;
+            this->left = NULL;
+        }
+};
+
+void preOrderTraversal(Node* root){
+    if(root == NULL){
+        return;
+    }
+    cout << root->val << " ";
+    preOrderTraversal(root->left);
+    preOrderTraversal(root->right);
+}
+
+void inOrderTraversal(Node* root){
+    if(root == NULL) return;
+    inOrderTraversal(root->left);
+    cout << root->val << " ";
+    inOrderTraversal(root->right);
+}
+int main() {
+    Node* root = new Node(10);
+    Node* a = new Node(20);
+    Node* b = new Node(30);
+    Node* c = new Node(40);
+    Node* d = new Node(50);
+    Node* e = new Node(60);
+    
+    root->left = a;
+    root->right = b;
+    a->left = c;
+    b->left = d;
+    b->right = e;
+
+    preOrderTraversal(root);
+    cout << endl;
+    inOrderTraversal(root);
+    return 0;
+}
