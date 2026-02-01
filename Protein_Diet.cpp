3f8c1d8292475198f720c22ecc171b2393118871
asrahim88 <asrahim162@gmail.com>
2026-02-01 13:30:58 +0600

    Progtein Diet

---
+++ Protein_Diet.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int X, Y;
+	cin >> X >> Y;
+	
+	if(X>=Y){
+	    cout << "YES" << endl;
+	} else {
+	    cout << "NO" << endl;
+	}
+
+}
