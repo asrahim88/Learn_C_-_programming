45545a8a66276fff0edbe5510c0eaba651a9860c
asrahim88 <asrahim162@gmail.com>
2026-02-07 11:01:18 +0600

    problem solve

---
+++ 2000.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int N;
+    cin >> N;
+
+    int notes = N*2000;
+    int result = notes / 500;
+    cout << result << endl;
+    return 0;
+}
