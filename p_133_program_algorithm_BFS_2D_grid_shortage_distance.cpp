a818164b9a4cd28fe39233d9bd4abbc5d806005c
asrahim88 <asrahim162@gmail.com>
2026-02-15 12:17:56 +0600

    Shoratage Distance using BFS on 2D grid

---
+++ p_133_program_algorithm_BFS_2D_grid_shortage_distance.cpp
@@ -0,0 +1,63 @@
+#include <bits/stdc++.h>
+using namespace std;
+int row, col;
+char grid[105][105];
+bool visited[105][105];
+int shortageDistance[105][105];
+
+vector<pair<int,int>> movement = {{-1, 0}, {1, 0}, {0,1}, {0, -1}};
+
+bool valid(int r, int c){
+    if(r<0 || r>=row || c<0 || c>=col)
+        return false;
+    return true;
+}
+
+void bfs(int sourceI, int sourceJ){
+    queue<pair<int,int>> q;
+    q.push({sourceI, sourceJ});
+    visited[sourceI][sourceJ] = true;
+    shortageDistance[sourceI][sourceJ] = 0;
+
+    while (!q.empty())
+    {
+        pair<int,int> parentSource = q.front();
+        q.pop();
+        
+        int parentSourceI = parentSource.first;
+        int parentSourceJ = parentSource.second;
+        
+        // cout << parentSourceI << " " << parentSourceJ << endl;
+
+        for(int i =0 ;i<4; i++){
+            int childI = parentSourceI + movement[i].first;
+            int childJ = parentSourceJ + movement[i].second;
+
+            if(valid(childI, childJ) && !visited[childI][childJ]){
+                q.push({childI, childJ});
+                visited[childI][childJ] = true;
+                shortageDistance[childI][childJ] = shortageDistance[parentSourceI][parentSourceJ] + 1;
+            }
+        }
+    }   
+}
+
+int main() {
+    cin >> row >> col;
+    for(int i =0; i<row; i++){
+        for(int j =0; j<col; j++){
+            cin >> grid[i][j];
+        }   
+    }
+    
+    memset(visited, false, sizeof(visited));
+    memset(shortageDistance, -1, sizeof(shortageDistance));
+    
+    int sourceI, sourceJ, distanceI, distanceJ;
+    cin >> sourceI >> sourceJ >> distanceI >> distanceJ;
+
+    bfs(sourceI, sourceJ);
+    cout << shortageDistance[distanceI][distanceJ];
+    
+    return 0;
+}
