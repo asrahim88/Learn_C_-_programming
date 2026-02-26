d6694fe94c902eb578f27ec97b241218ff848e6d
asrahim88 <asrahim162@gmail.com>
2026-02-26 10:48:08 +0600

    Adjacency Matrix practice

--- p_139_program_Adjacency_matrix_undirected.cpp
+++ p_139_program_Adjacency_matrix_undirected.cpp
@@ -4,25 +4,21 @@ using namespace std;
 int main() {
     int n, e;
     cin >> n >> e;
-
-    int adj_matrix[n][n];
+    int adjMatrix[n][n];
     
-    memset(adj_matrix,0, sizeof(adj_matrix));
-    for(int i =0; i<n; i++)
-        for(int j =0; j<n; j++)
-            if(i==j)
-                adj_matrix[i][j] = 1;
+    memset(adjMatrix, 0, sizeof(adjMatrix));
 
     while(e--){
         int a, b;
         cin >> a >> b;
-        adj_matrix[a][b] = 1;
-        adj_matrix[b][a] = 1;
+        adjMatrix[a][b] = 1;
+        adjMatrix[b][a] = 1;
     }
 
-    for(int i =0; i<n; i++){
+    for(int i =0; i<n; i++)
+    {
         for(int j =0; j<n; j++){
-            cout << adj_matrix[i][j] << " ";
+            cout << adjMatrix[i][j];
         }
         cout << endl;
     }
