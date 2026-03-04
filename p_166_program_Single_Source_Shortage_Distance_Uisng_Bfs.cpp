7944ffc4720d2f534d1e440d84cdf61d79efbf7d
asrahim88 <asrahim162@gmail.com>
2026-03-04 17:39:55 +0600

    Single Source Shorage Distance Using BFS

---
+++ p_166_program_Single_Source_Shortage_Distance_Uisng_Bfs.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+using namespace std;
+vector<int> adjList[150];
+bool visited[150];
+int level[150];
+
+void bfs(int src){
+    queue<int> q;
+    q.push(src);
+    visited[src] = true;
+    level[src] = 0; 
+
+    while (!q.empty())
+    {
+        int parent = q.front();
+        q.pop();
+
+        for(int child: adjList[parent]){
+            if(!visited[child]){
+                q.push(child);
+                visited[child] = true;
+                level[child] = level[parent] + 1;
+            }
+        }
+    }
+    
+}
+
+int main() {
+    int n, e;
+    cin >> n >> e;
+    memset(visited, false, sizeof(visited));
+    memset(level, -1, sizeof(level));
+
+    while(e--){
+        int a, b;
+        cin >> a >> b;
+        adjList[a].push_back(b);
+        adjList[b].push_back(a);
+    }
+
+    int src, dest;
+    cin >> src >> dest;
+
+    bfs(src);
+
+    cout<< level[dest] << endl;
+    return 0;
+}
