7e38af6642e30d4a4f9cbf2f8bb406a184651857
asrahim88 <asrahim162@gmail.com>
2026-03-04 16:12:47 +0600

    DSU Union

---
+++ p_162_program_DSU_Unio.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+int parent[105];
+int group_size[105];
+
+int findOperation(int node){
+    if(parent[node] == -1)
+        return node;
+    int leader = findOperation(parent[node]);
+    parent[node] = leader;
+    return leader;
+}
+void dsu_union(int node1, int node2){
+    int leader1 = findOperation(node1);
+    int leader2 =  findOperation(node2);
+
+    if(group_size[leader1]>= group_size[leader2]){
+        parent[leader2] = leader1;
+        group_size[leader1] += group_size[leader2];
+    }else{
+        parent[leader1] = leader2;
+        group_size[leader2] += group_size[leader1];
+    }
+    
+}
+int main() {
+    memset(parent, -1, sizeof(parent));
+    memset(group_size, 1, sizeof(group_size));
+
+    dsu_union(1, 2);
+    
+    for(int i=0; i<6; i++){
+        cout << i << " " << parent[i] << endl;
+    }
+    return 0;
+}
