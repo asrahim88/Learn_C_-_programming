91551935d3b2d2ffe799ca0621a162a70d324335
asrahim88 <asrahim162@gmail.com>
2026-03-07 10:50:35 +0600

    Find using recusion without optimize

---
+++ p_176_program_Find_using_recursion_with_optimize.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+int parent[150];
+
+int findRecursion(int node){
+    if(parent[node] == -1)
+        return node;
+    int leader = findRecursion(parent[node]);
+    parent[node] = leader;
+    return leader;
+}
+
+int main() {
+    memset(parent, -1, sizeof(parent));
+
+    parent[0] = 1;
+    parent[1] = -1;
+    parent[2] = 1;
+    parent[3] = 1;
+    parent[4] = 5;
+    parent[5] = 3;
+
+    return 0;
+}
