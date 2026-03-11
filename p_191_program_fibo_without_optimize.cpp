471992ced8e46951fc05a5015201b19bc9f6c56e
asrahim88 <asrahim162@gmail.com>
2026-03-11 22:53:36 +0600

    fibonacci using recursion without optimize version

---
+++ p_191_program_fibo_without_optimize.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int fib(int n){
+    if(n<2)
+        return n;
+    return fib(n-2) + fib(n-1);
+}
+
+int main() {
+    int n;
+    cin >> n;
+    cout << fib(n) << endl;
+    return 0;
+}
