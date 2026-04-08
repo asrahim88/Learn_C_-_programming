5446b81e2574b0316726b82c63211c289309d7ac
asrahim88 <asrahim162@gmail.com>
2026-04-08 22:23:36 +0600

    Cake Discount

---
+++ Cake_Discount.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    if(n<5){
+        cout << n * 100 << endl;
+    }else {
+        cout << n * 85 << endl;
+    }
+    return 0;
+}
