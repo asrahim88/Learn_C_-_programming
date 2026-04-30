53c230ba3cb4af19a59606bb24b0deb506b5b80a
asrahim88 <asrahim162@gmail.com>
2026-04-30 23:05:27 +0600

    Party Monster problem solve

---
+++ B_Party_Monster.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t;
+    cin >> t;
+    while(t--){
+        int n;
+        cin >> n;
+        string s;
+        cin >> s;
+
+        int a = 0;
+        int b = 0;
+        for(int i=0;i<n; i++){
+            if(s[i] == ')')
+                a++;
+            else 
+                b++;
+        }
+        if(a == b)
+            cout << "YES" <<endl;
+        else 
+            cout << "NO" <<endl;
+    }
+    return 0;
+}
