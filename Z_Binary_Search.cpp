fef387d4de1854548062037bef14a8950a9c0257
asrahim88 <asrahim162@gmail.com>
2026-09-24 23:13:47 +0600

    Z_Binary_Search problem solved

--- Z_Binary_Search.cpp
+++ Z_Binary_Search.cpp
@@ -2,45 +2,55 @@
 using namespace std;
 
 int main() {
-    int N, Q;
-    cin >> N >> Q;
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
 
-    int a[N];
-    for(int i =0; i<N; i++) {
-        cin >> a[i];
-    }
-
-    sort(a, a+N);
-
-    while(Q--){
-        int x;
-        cin >> x;
-
-        int flag = 0;
-
-        int l = 0;
-        int r = N-1;
-
-        
-        while(l<=r){
-            int mid = (l+r) / 2;
-            if(a[mid] == x) {
-                flag = 1;
-                break;
-            }else if(a[mid] > x){
-                r = mid-1;
-            }else {
-                l = mid+1;
-            }
-        }
-
-        if(flag == 1){
-            cout << "found\n";
-        }else {
-            cout << "not found\n";
-        }
-
-    }
     
     return 0;
 }
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// int main() {
+//     int N, Q;
+//     cin >> N >> Q;
+
+//     int a[N];
+//     for(int i =0; i<N; i++) {
+//         cin >> a[i];
+//     }
+
+//     sort(a, a+N);
+
+//     while(Q--){
+//         int x;
+//         cin >> x;
+
+//         int flag = 0;
+
+//         int l = 0;
+//         int r = N-1;
+
+        
+//         while(l<=r){
+//             int mid = (l+r) / 2;
+//             if(a[mid] == x) {
+//                 flag = 1;
+//                 break;
+//             }else if(a[mid] > x){
+//                 r = mid-1;
+//             }else {
+//                 l = mid+1;
+//             }
+//         }
+
+//         if(flag == 1){
+//             cout << "found\n";
+//         }else {
+//             cout << "not found\n";
+//         }
+
+//     }
+    
+//     return 0;
+// }
