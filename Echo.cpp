2536640f55501195262ce0e222459296ce37a217
asrahim88 <asrahim162@gmail.com>
2026-09-24 23:15:59 +0600

    Echo problem solved

---
+++ Echo.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	string s;
+	cin >>s;
+	if(s[0] == s[2] && s[1] == s[3])
+	    cout << "Yes" << endl;
+	else 
+	    cout << "No" << endl;
+}
